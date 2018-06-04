cf5d12e9203677e96525e55ca984188096bafe11
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-04 14:52:00 -0400

    changed home page from group tasks index to separate index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
 
-  root to: "group_tasks#index"
+  root to: "application#index"
 
   resources :group_tasks
 
