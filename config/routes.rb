49cf6ed5c87d029ce447905503184f21673c9256
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-04 13:04:03 -0400

    made group_tasks root route and created a group tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  root to: "group_tasks#index"
 
+  
 end
