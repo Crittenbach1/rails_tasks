20c3e46c0b1453d314582771054a715c7751fd97
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-04 15:05:55 -0400

    added list of my tasks to user index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def index
+    @my_tasks = []
+    Task.all.each do |t|
+       if t.user_id == current_user.id
+          @my_tasks << t
+       end
+    end
+    
   end
 
 end
