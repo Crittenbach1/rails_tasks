c0f6a3a49d3359dd2cfb7decdedbb4bb5d262d43
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-04 13:48:43 -0400

    set up create path in group task controller

--- app/controllers/group_tasks_controller.rb
+++ app/controllers/group_tasks_controller.rb
@@ -10,12 +10,17 @@ class GroupTasksController < ApplicationController
   end
 
   def new
-    @group_task = GroupTask.new 
+    @group_task = GroupTask.new
     @task = Task.new
   end
 
   def create
-
+    @group_task = GroupTask.new(group_task_params)
+    if @group_task.save
+      redirect_to group_task_path(@group_task)
+    else
+      render :new
+    end
   end
 
   def edit
