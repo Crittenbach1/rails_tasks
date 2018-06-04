db3c0d6504863a47de4019ce73ce169baf84747c
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-04 13:50:48 -0400

    added form input validations for group tasks and tasks

--- app/models/group_task.rb
+++ app/models/group_task.rb
@@ -1,6 +1,8 @@
 class GroupTask < ApplicationRecord
   has_many :tasks
   has_many :users, through: :tasks
+  validates :name, presence: true
+  validates :due_date, presence: true
 
   def task=(task_attributes)
     if task_attributes[:name].present?
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :group_task
+  validates :name, presence: true
 end
