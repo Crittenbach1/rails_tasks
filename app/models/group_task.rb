a8c1d7fd4449582d87ad80ad85448ca8b3850e73
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-04 13:46:21 -0400

    created group tasks task model methdo for creating tasks

--- app/models/group_task.rb
+++ app/models/group_task.rb
@@ -2,4 +2,11 @@ class GroupTask < ApplicationRecord
   has_many :tasks
   has_many :users, through: :tasks
 
+  def task=(task_attributes)
+    if task_attributes[:name].present?
+        task = Task.create(task_attributes)
+        self.tasks << task
+      end
+  end
+
 end
