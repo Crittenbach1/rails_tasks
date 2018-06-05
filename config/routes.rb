3e8b9cdbbe9c8aa9c3c91ff8db57c20671be225d
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-05 13:59:10 -0400

    tried a nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'registrations' }
 
   root to: "tasks#index"
-
-  resources :group_tasks
-
-  resources :tasks
+  resources :group_tasks, :users, :tasks
 
 
 
